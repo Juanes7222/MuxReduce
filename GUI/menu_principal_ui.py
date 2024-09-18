48800d17d044b45b8653b84b0d711af8bb34f5fb
Juanes7222 <133039607+Juanes7222@users.noreply.github.com>
2024-09-18 14:33:41 -0600

    Add files via upload

--- GUI/menu_principal_ui.py
+++ GUI/menu_principal_ui.py
@@ -23,7 +23,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(fr"{BASE_DIR}")
 from utils import procesar_variables, to_list, validar_pesos, obtener_cant_vars
-from GUI.salida_ui import SalidaMenu
+# from GUI.salida_ui import SalidaMenu
 import controller
 import src.img.fondo_rc
 
@@ -72,13 +72,7 @@ class Ui_MainWindow(QMainWindow):
 "                                   border-radius: 8px;\n"
 "}")
         # self.calcular
-        self.calcular.clicked.connect(self.event_calc)
-        self.label_cantidad = QLabel(self.centralwidget)
-        self.label_cantidad.setObjectName(u"label_cantidad")
-        self.label_cantidad.setGeometry(QRect(10, 260, 201, 91))
-        self.label_cantidad.setFont(font1)
-        self.label_cantidad.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label_cantidad.setWordWrap(True)
+        # self.calcular.clicked.connect(self.event_calc)
         self.minterminos = QLineEdit(self.centralwidget)
         self.minterminos.setObjectName(u"minterminos")
         self.minterminos.setGeometry(QRect(220, 190, 541, 51))
@@ -89,20 +83,24 @@ class Ui_MainWindow(QMainWindow):
         self.minterminos.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.minterminos.setCursorMoveStyle(Qt.CursorMoveStyle.VisualMoveStyle)
         self.minterminos.setClearButtonEnabled(True)
-        self.cant_variables = QLineEdit(self.centralwidget)
-        self.cant_variables.setObjectName(u"cant_variables")
-        self.cant_variables.setGeometry(QRect(220, 280, 541, 51))
-        self.cant_variables.setFont(font3)
-        self.cant_variables.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.cant_variables.setCursorMoveStyle(Qt.CursorMoveStyle.VisualMoveStyle)
-        self.cant_variables.setClearButtonEnabled(True)
-        self.variables = QLineEdit(self.centralwidget)
+        self.variables = QLabel(self.centralwidget)
         self.variables.setObjectName(u"variables")
         self.variables.setGeometry(QRect(220, 370, 541, 51))
         self.variables.setFont(font3)
         self.variables.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.variables.setCursorMoveStyle(Qt.CursorMoveStyle.VisualMoveStyle)
-        self.variables.setClearButtonEnabled(True)
+        self.variables.setWordWrap(True)
+        # self.variables.setCursorMoveStyle(Qt.CursorMoveStyle.VisualMoveStyle)
+        # self.variables.setClearButtonEnabled(True)
+        self.variables.setStyleSheet(u"Qlabel{\n"
+                            "background-color: #4CAF50;\n"
+                            "border: none;\n"
+                            "color: white;\n"
+                            "padding: 10px 20px;\n"
+                            "text-align: center;\n"
+                            "text-decoration: none;\n"
+                            "font-size: 16px;\n"
+                            "border-radius: 10px;\n"
+                                     "}")
         # self.minterminos.setAcceptRichText(True)
         self.label_variabls = QLabel(self.centralwidget)
         self.label_variabls.setObjectName(u"label_variabls")
@@ -118,8 +116,7 @@ class Ui_MainWindow(QMainWindow):
         self.Titulo_principal.raise_()
         self.label_minterminos.raise_()
         self.calcular.raise_()
-        self.label_cantidad.raise_()
-        self.cant_variables.raise_()
+        # self.cant_variables.raise_()
         self.minterminos.raise_()
         self.label_variabls.raise_()
         self.variables.raise_()
@@ -145,11 +142,8 @@ class Ui_MainWindow(QMainWindow):
         
         self.retranslateUi(self)
         # self.fondo_pantalla.setScaledContents(True)
-        size_policy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         # self.fondo_pantalla.setSizePolicy(size_policy)
-        self.cant_variables.textChanged.connect(self.verificar_cant)
         self.minterminos.textChanged.connect(self.cambio_vars)
-        self.variables.textChanged.connect(self.cambio_variables)
         # self.minterminos.textEdited.connect(self.cambio_vars)
 
         QMetaObject.connectSlotsByName(self)
@@ -160,23 +154,19 @@ class Ui_MainWindow(QMainWindow):
         self.Titulo_principal.setText(QCoreApplication.translate("MainWindow", u"Mc. Closkey", None))
         self.label_minterminos.setText(QCoreApplication.translate("MainWindow", u"Minterminos", None))
         self.calcular.setText(QCoreApplication.translate("MainWindow", u"Calcular", None))
-        self.label_cantidad.setText(QCoreApplication.translate("MainWindow", u"Cantidad de terminos", None))
+        # self.label_cantidad.setText(QCoreApplication.translate("MainWindow", u"Cantidad de terminos", None))
         self.label_variabls.setText(QCoreApplication.translate("MainWindow", u"Variables", None))
         # self.fondo_pantalla.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>|<img src=\":/fondo_pantalla/94-948666_m.jpg\"/></p></body></html>", None))
         self.minterminos.setPlaceholderText(QCoreApplication.translate("MainWindow", u"1,2,3,4,5,6,...", None))
-        self.cant_variables.setPlaceholderText(QCoreApplication.translate("MainWindow", u"# de variables", None))
-        self.variables.setPlaceholderText(QCoreApplication.translate("MainWindow", u"ABCD", None))
+        # self.variables.setPlaceholderText(QCoreApplication.translate("MainWindow", u"ABCD", None))
     # retranslateUi
     
     def resizeEvent(self, event):
-        # Redimensionar la imagen de fondo cuando la ventana cambie de tamaño
-        # pixmap = QPixmap("ruta/a/tu/imagen.jpg")  # Vuelve a cargar la imagen
         scaled_pixmap = self.pixmap.scaled(self.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
         palette = self.palette()
         palette.setBrush(QPalette.Window, QBrush(scaled_pixmap))
         self.setPalette(palette)
 
-        # Llamar al evento de redimensionamiento original
         super().resizeEvent(event)
     
     def cambio_vars(self):
@@ -187,31 +177,10 @@ class Ui_MainWindow(QMainWindow):
             return
         else:
             self.volver_estilo(self.minterminos)
-
-        if self.cant_variables.text().strip() != "":
-            self.verificar_cant()
-            return
         
-        self.variables.setPlaceholderText("".join(variables))
+        self.variables.setText("".join(variables))
         self.volver_estilo(self.minterminos)
-        
-    def verificar_cant(self):
-        if self.cant_variables.text().strip() == "":
-            self.volver_estilo(self.cant_variables)
-            self.cambio_vars()
-            return
-        try:
-            cant = int(self.cant_variables.text())
-            minterminos = to_list(self.minterminos.text())
-            cant_vars = obtener_cant_vars(minterminos)
-            if cant < cant_vars:
-                self.error_cant("", self.cant_variables)     
-            else:
-                self.variables.setPlaceholderText(ascii_uppercase[:cant])
-                self.volver_estilo(self.cant_variables)
-                
-        except ValueError:
-            self.error_cant("", self.cant_variables)
+    
         
     def error_cant(self, mensaje, obj):
         nuevo_estilo = self.estilo_original + "QLineEdit { border: 3px solid red; }"
@@ -222,55 +191,24 @@ class Ui_MainWindow(QMainWindow):
     
     def obtener_valores(self):
         minterminos = self.minterminos.text()
-        if minterminos.strip() == "":
+        if not validar_pesos(minterminos):
             self.error_cant("", self.minterminos)
             return
-        elif not validar_pesos(minterminos):
-            self.error_cant("", self.minterminos)
-            return
-        variables = self.variables.text()
-        variables = variables.strip()
-        if variables == "":
-            variables, _ = procesar_variables(minterminos, self.cant_variables.text().strip())
-        else:
-            variables = list(variables)
-            if len(set(variables)) < len(variables):
-                self.error_cant("", self.variables)
-                return
-        cant_terminos = self.cant_variables.text().strip()
-        if cant_terminos == "":
-            cant_terminos = len(variables)
-        else:
-            cant_terminos = int(cant_terminos)
-        if cant_terminos != len(variables):
-            self.error_cant("", self.variables)
+        
         params = locals()
         params.pop("self")
         
         resultado = controller.main(**params)
         return resultado
     
-    def cambio_variables(self):
-        variables = self.variables.text()
-        variables = variables.strip()
-        cant = self.cant_variables.text().strip()
-        if cant != "":
-            length = int(cant)
-        else:
-            length = len(variables)
-        if length != obtener_cant_vars(self.minterminos.text().strip().split(",")):
-            self.error_cant("", self.variables)
-        else:
-            self.volver_estilo(self.variables)
-    
-    def event_calc(self, event):
-        resultado = self.obtener_valores()
-        if not resultado:
-            return
-        self.hide()
+    # def event_calc(self, event):
+    #     resultado = self.obtener_valores()
+    #     if not resultado:
+    #         return
+    #     self.hide()
         
-        self.salida = SalidaMenu(self)
-        self.salida.show()
+    #     self.salida = SalidaMenu(self)
+    #     self.salida.show()
 
 if __name__ == "__main__":
     import sys
